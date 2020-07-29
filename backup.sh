3ce0986f86be31415f50b7f9366e568adc1a92a6
Tobias Fischer <info@tobiasfischer.info>
2020-07-30 09:16:16 +1000

    No need for sshpass, proper handling of iptables

--- backup.sh
+++ backup.sh
@@ -48,7 +48,7 @@ then
   # will _not_ source .bashrc, which in turn won't source /opt/ros/[distro]/setup.bash
   # therefore we check the /opt/ros folder for the appropriate distro folder and use that
   # if there's more than one folder we pick the last one
-  ROSDISTRO=$(echo "ls -rt /opt/ros | tail -1" | sshpass -p "$PASSWORD" ssh -T $USERNAME@$HOST | tail -1)
+  ROSDISTRO=$(echo "ls -rt /opt/ros | tail -1" | ssh -T $USERNAME@$HOST | tail -1)
   echo "ROS distro is $ROSDISTRO"
   echo "$ROSDISTRO" > ./ROS_DISTRO
 
@@ -62,52 +62,54 @@ then
   ############################ BACKUP #############################
 
   echo "Copying udev rules"
-  sshpass -p "$PASSWORD" scp -r $USERNAME@$HOST:/etc/udev/rules.d .
+  scp -r $USERNAME@$HOST:/etc/udev/rules.d .
 
   echo "Copying Network Setup"
-  sshpass -p "$PASSWORD" scp $USERNAME@$HOST:/etc/network/interfaces .
-  sshpass -p "$PASSWORD" scp $USERNAME@$HOST:/etc/hostname .
-  sshpass -p "$PASSWORD" scp $USERNAME@$HOST:/etc/hosts .
+  scp $USERNAME@$HOST:/etc/network/interfaces .
+  scp $USERNAME@$HOST:/etc/hostname .
+  scp $USERNAME@$HOST:/etc/hosts .
 
   echo "Copying IPTables"
-  sshpass -p "$PASSWORD" scp $USERNAME@$HOST:/etc/iptables .
+  scp -r $USERNAME@$HOST:/etc/iptables .
 
   echo "Copying Bringup"
-  sshpass -p "$PASSWORD" scp $USERNAME@$HOST:/etc/ros/setup.bash .
-  sshpass -p "$PASSWORD" scp -r $USERNAME@$HOST:/etc/ros/$ROSDISTRO/ros.d .
+  scp $USERNAME@$HOST:/etc/ros/setup.bash .
+  scp -r $USERNAME@$HOST:/etc/ros/$ROSDISTRO/ros.d .
   mkdir -p usr/sbin
-  sshpass -p "$PASSWORD" scp -r $USERNAME@$HOST:/usr/sbin/*start usr/sbin
-  sshpass -p "$PASSWORD" scp -r $USERNAME@$HOST:/usr/sbin/*stop usr/sbin
+  scp -r $USERNAME@$HOST:/usr/sbin/*start usr/sbin
+  scp -r $USERNAME@$HOST:/usr/sbin/*stop usr/sbin
 
   echo "Copying RosDep sources"
-  sshpass -p "$PASSWORD" scp -r $USERNAME@$HOST:/etc/ros/rosdep/ .
+  scp -r $USERNAME@$HOST:/etc/ros/rosdep/ .
 
   echo "Copying rclocal"
-  sshpass -p "$PASSWORD" scp -r $USERNAME@$HOST:/etc/rc.local .
+  scp -r $USERNAME@$HOST:/etc/rc.local .
 
   echo "Copying pip packages"
   # strip the first 2 lines from pip list output; they're headers we don't need!
-  echo "$SSH_SOURCE_CMD && pip list | tail -n +3 > /tmp/pip.list" | sshpass -p "$PASSWORD" ssh -T $USERNAME@$HOST
-  sshpass -p "$PASSWORD" scp -r $USERNAME@$HOST:/tmp/pip.list .
-  echo "rm /tmp/pip.list" | sshpass -p "$PASSWORD" ssh -T $USERNAME@$HOST
+  echo "$SSH_SOURCE_CMD && pip list | tail -n +3 > /tmp/pip.list" | ssh -T $USERNAME@$HOST
+  scp -r $USERNAME@$HOST:/tmp/pip.list .
+  echo "rm /tmp/pip.list" | ssh -T $USERNAME@$HOST
 
   echo "Copying Systemd"
-  sshpass -p "$PASSWORD" scp -r $USERNAME@$HOST:/etc/systemd/system .
+  scp -r $USERNAME@$HOST:/etc/systemd/system .
 
   echo "Copying user groups"
-  echo "groups" | sshpass -p "$PASSWORD" ssh -T $USERNAME@$HOST | tail -1 > groups
+  echo "groups" | ssh -T $USERNAME@$HOST | tail -1 > groups
 
   echo "Copying APT sources & packages"
-  sshpass -p "$PASSWORD" scp -r $USERNAME@$HOST:/etc/apt/sources.list.d .
-  echo "apt-mark showmanual > /tmp/installed_pkgs.list" | sshpass -p "$PASSWORD" ssh -T $USERNAME@$HOST
-  sshpass -p "$PASSWORD" scp -r $USERNAME@$HOST:/tmp/installed_pkgs.list .
-  echo "rm /tmp/installed_pkgs.list" | sshpass -p "$PASSWORD" ssh -T $USERNAME@$HOST
+  scp -r $USERNAME@$HOST:/etc/apt/sources.list.d .
+  echo "Copying APT sources & packages2"
+  echo "apt-mark showmanual > /tmp/installed_pkgs.list" | ssh -T $USERNAME@$HOST
+  scp -r $USERNAME@$HOST:/tmp/installed_pkgs.list .
+  echo "Copying APT sources & packages3"
+  echo "rm /tmp/installed_pkgs.list" | ssh -T $USERNAME@$HOST
 
   echo "Cleaning up bag"
-  echo "rm auto_backup*.bag" | sshpass -p "$PASSWORD" ssh -T $USERNAME@$HOST
+  echo "rm auto_backup*.bag" | ssh -T $USERNAME@$HOST
 
   echo "Copying Home Folder"
-  sshpass -p "$PASSWORD" scp -r $USERNAME@$HOST:~ .
+  scp -r $USERNAME@$HOST:~ .
 
   cd ..
   echo "Done Transfer"
